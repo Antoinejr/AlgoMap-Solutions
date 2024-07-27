f8b3c74ac2f75e87930ca02865181e7a646937b7
Anthony <Antoinejr11@outlook.com>
2024-07-27 19:47:19 +0200

    Update 14. Longest Common Prefix.cpp

--- Arrays-and-Strings/14. Longest Common Prefix.cpp
+++ Arrays-and-Strings/14. Longest Common Prefix.cpp
@@ -13,13 +13,24 @@ private:
     }
 public:
     std::string longestCommonPrefix(std::vector<std::string>& strs) {
-        int N = strs.size();
-        std::string result = strs[0];
+        /**
+        *int N = strs.size();
+        *std::string result = strs[0];
 
-        for(int i = 1; i < N; i++) {
-            result = findCommonPrefix(result, strs[i]);
-            if(result == "") break;
-        }
-        return result;        
+        *for(int i = 1; i < N; i++) {
+        *    result = findCommonPrefix(result, strs[i]);
+        *    if(result == "") break;
+        *}
+        *return result;
+        **/
+        
+        std::sort(strs.begin(), strs.end());
+        std:string s1 = strs[0], s2 = strs[strs.size()-1];
+        int idx = 0;
+        while(idx < std::min(s1.size(), s2.size())) {
+            if(s1[idx] != s2[idx]) break;
+            idx++;
+        }  
+        return s1.substr(0,idx);   
     }
 };
