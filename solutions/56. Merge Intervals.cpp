2d0c3365c48c22057fd0c4c269e4aeb7aafe98ee
Deji <antoinejr11@outlook.com>
2024-07-28 17:44:53 +0200

    updated 56

--- solutions/56. Merge Intervals.cpp
+++ solutions/56. Merge Intervals.cpp
@@ -1,8 +1,10 @@
+#include <vector>
+#include <algorithm>
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> answer;
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>&b)
+    std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
+        std::vector<std::vector<int>> answer;
+        std::sort(intervals.begin(), intervals.end(), [](const std::vector<int>& a, const std::vector<int>&b)
         {
             return a[0] < b[0];
         });
@@ -10,7 +12,7 @@ public:
 
         for(int i = 1; i < intervals.size(); i++) {
             if(answer.back()[1] >= intervals[i][0]) {
-                answer.back()[1] = max(answer.back()[1], intervals[i][1]);
+                answer.back()[1] = std::max(answer.back()[1], intervals[i][1]);
             }
             else answer.push_back(intervals[i]);
         }
