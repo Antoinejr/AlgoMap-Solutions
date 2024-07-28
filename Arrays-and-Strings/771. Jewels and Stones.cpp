b42430bbdc1cbf8de2f04f303fa29a57bb1c468a
Deji <antoinejr11@outlook.com>
2024-07-28 17:35:50 +0200

    updated 228 and 771

--- Arrays-and-Strings/771. Jewels and Stones.cpp
+++ Arrays-and-Strings/771. Jewels and Stones.cpp
@@ -1,7 +1,9 @@
+#include <unordered_set>
+#include <string>
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) {
-        unordered_set<char> jewelMap(jewels.begin(), jewels.end());
+    int numJewelsInStones(std::string jewels, std::string stones) {
+        std::unordered_set<char> jewelMap(jewels.begin(), jewels.end());
         int result = 0;
         for(const char& c : stones) {
             if(jewelMap.find(c) != jewelMap.end()) result++;
